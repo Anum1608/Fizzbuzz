77ba26ba9e41864f34424cb8e90b14a2e40d732e
anum1608 <riaz.anum@outlook.com>
2024-08-05 11:40:53 +0100

    print bang if multiple of 7, FizzBang, BuzzBang , FizzBuzzBang, and print Bong if multiple of 11

--- Program.cs
+++ Program.cs
@@ -7,13 +7,22 @@ int maxNumberInt = Convert.ToInt32(maxNumber);
 Console.WriteLine($"printing from 1 to {maxNumber}");
 for (int i = 1; i <= maxNumberInt; i = i + 1) 
 {
-   if (i % 3 == 0 && i % 5 == 0){
-        Console.WriteLine("FizzBuzz");
-   }else if(i % 5 == 0){
-        Console.WriteLine("Buzz");
-   }else if (i % 3 == 0){
-        Console.WriteLine("Fizz");
-   } else{
+   if (i % 3 == 0 || i % 5 == 0){
+        if (i % 3 == 0){
+            Console.Write("Fizz");
+        }
+        if(i % 5 == 0){
+            Console.Write("Buzz");
+        }
+        if(i % 7 == 0){
+            Console.Write("Bang");
+        }
+        Console.WriteLine("");
+   } else if(i % 7 == 0){
+        Console.WriteLine("Bang");
+   } else if(i % 11 == 0){
+        Console.WriteLine("Bong");
+    }else{
         Console.WriteLine(i);
    }
 }
--- obj/Debug/net8.0/FizzBuzz.AssemblyInfo.cs
+++ obj/Debug/net8.0/FizzBuzz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6682765c3daf1a272a4d3fc4a4ffc11fc95c8c32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42944046e76cfddea55ab1fd5f654a4241ad9f6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
