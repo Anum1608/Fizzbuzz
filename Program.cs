252fdf8356b8731e9acf6018e89b94cfff0cced8
anum1608 <riaz.anum@outlook.com>
2024-08-05 12:06:34 +0100

    Reverse all the printing order of Fezz Fizz Buzz Bang Bong for multiples of 17

--- Program.cs
+++ Program.cs
@@ -7,7 +7,37 @@ int maxNumberInt = Convert.ToInt32(maxNumber);
 Console.WriteLine($"printing from 1 to {maxNumber}");
 for (int i = 1; i <= maxNumberInt; i = i + 1) 
 {
-   if (i % 3 == 0 || i % 5 == 0){
+
+   if(i % 17 == 0){
+    if (i % 3 == 0 || i % 5 == 0){
+        if(i % 7 == 0){
+            Console.Write("Bang");
+        }
+        if(i % 5 == 0){
+            Console.Write("Buzz");
+        }
+        if (i % 13 == 0){
+            Console.Write("Fezz");
+        }
+        if (i % 3 == 0){
+            Console.Write("Fizz");
+        }
+        Console.WriteLine("");
+    } else if(i % 7 == 0){
+        Console.WriteLine("Bang");
+    } else if(i % 11 == 0 || i % 13 == 0){
+        if(i % 11 == 0){
+            Console.Write("Bong");
+        }
+        if (i % 13 == 0){
+            Console.Write("Fezz");
+        }
+        Console.WriteLine("");
+    }else{
+        Console.WriteLine(i);
+    }
+   }else{   
+    if (i % 3 == 0 || i % 5 == 0){
         if (i % 3 == 0){
             Console.Write("Fizz");
         }
@@ -21,9 +51,9 @@ for (int i = 1; i <= maxNumberInt; i = i + 1)
             Console.Write("Bang");
         }
         Console.WriteLine("");
-   } else if(i % 7 == 0){
+    } else if(i % 7 == 0){
         Console.WriteLine("Bang");
-   } else if(i % 11 == 0 || i % 13 == 0){
+    } else if(i % 11 == 0 || i % 13 == 0){
         if (i % 13 == 0){
             Console.Write("Fezz");
         }
@@ -33,5 +63,6 @@ for (int i = 1; i <= maxNumberInt; i = i + 1)
         Console.WriteLine("");
     }else{
         Console.WriteLine(i);
+    }
    }
 }
