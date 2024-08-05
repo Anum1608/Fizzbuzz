4ff1379ce6bb63d16043ead8f8130602dfd803ed
anum1608 <riaz.anum@outlook.com>
2024-08-05 11:03:37 +0100

    print number from 1 to 100, print Fizz if number is a multiple of 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("print from 1 to 100");
+for (int i = 1; i <= 100; i = i + 1) 
+{
+  Console.Write(i + " ");
+   if (i % 3 == 0){
+        Console.WriteLine("Fizz");
+   } else{
+        Console.WriteLine("");
+   }
+}
--- obj/Debug/net8.0/FizzBuzz.AssemblyInfo.cs
+++ obj/Debug/net8.0/FizzBuzz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6116b5b290b3bb83bd4490acd22ab2ba2d01859")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+061ecb436cfbbf5417d858ce2bb477c231bd8ad8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
